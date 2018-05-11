d6f7e62e16a40438e8f50aac13b7c4d79a1395fd
lbcong <lbcong15@gmail.com>
2018-05-11 11:30:48 +0700

    fix-seeder-model

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -5,6 +5,7 @@ use Faker\Factory as Faker;
 use App\Models\EmployeeType;
 use App\Models\Role;
 use App\Models\Team;
+use App\Models\Employee;
 
 class EmployeeSeeder extends Seeder
 {
@@ -18,26 +19,29 @@ class EmployeeSeeder extends Seeder
         $faker = Faker::create();
         $employee_types = EmployeeType::pluck('id');
         $teams = Team::pluck('id');
-        $roles = Role::pluck('id');
+        $roles = Role::where('name','<>','PO')->pluck('id');
 
-        DB::table('employees')->insert([
-            'email' => 'user1@gmail.com',
-            'password' => bcrypt('12345678'),
-            'name' => $faker->name,
-            'birthday' => $faker->dateTimeBetween('-40 years', '-10 years'),
-            'gender' => rand(1, 2),
-            'mobile' =>'0905123421',
-            'address' => $faker->address,
-            'marital_status' => rand(1, 2),
-            'startwork_date' => $faker->dateTimeBetween('-2 years', '-2 months'),
-            'endwork_date' => $faker->dateTimeBetween('-1 months', 'now'),
-            'curriculum_vitae' => 1,
-            'is_employee' => 1,
-            'avatar' => $faker->imageUrl(480, 600),
-            'employee_type_id' => $employee_types[rand(0, count($employee_types) - 1)],
-            'team_id' => $teams[rand(0, count($teams) - 1)],
-            'role_id' => $roles[rand(0, count($roles) - 1)],
-        ]);
+        if (Employee::where('email', '=', 'admin@gmail.com')->get()->isEmpty()) {
+                DB::table('employees')->insert([
+                    'email' => 'admin@gmail.com',
+                    'password' => bcrypt('12345678'),
+                    'name' => $faker->name,
+                    'birthday' => $faker->dateTimeBetween('-40 years', '-10 years'),
+                    'gender' => rand(1, 2),
+                    'mobile' => '0905123421',
+                    'address' => $faker->address,
+                    'marital_status' => rand(1, 2),
+                    'startwork_date' => $faker->dateTimeBetween('-2 years', '-2 months'),
+                    'endwork_date' => $faker->dateTimeBetween('-1 months', 'now'),
+                    'curriculum_vitae' => 1,
+                    'is_employee' => 1,
+                    'avatar' => $faker->imageUrl(480, 600),
+                    'employee_type_id' => $employee_types[rand(0, count($employee_types) - 1)],
+                    'team_id' => $teams[rand(0, count($teams) - 1)],
+                    'role_id' => $roles[rand(0, count($roles) - 1)],
+                ]);
+        }
+
 
         foreach (range(1, 40) as $index) {
             DB::table('employees')->insert([
@@ -46,7 +50,7 @@ class EmployeeSeeder extends Seeder
                 'name' => $faker->name,
                 'birthday' => $faker->dateTimeBetween('-40 years', '-10 years'),
                 'gender' => rand(1, 2),
-                'mobile' =>'0905123421',
+                'mobile' => '0905123421',
                 'address' => $faker->address,
                 'marital_status' => rand(1, 2),
                 'startwork_date' => $faker->dateTimeBetween('-2 years', '-2 months'),
