aaffb42ea0eea97e0085ff2eb84f4b5e15ad81d4
CongPhuc <phucb1506@gmail.com>
2018-05-21 18:08:59 +0700

    fix label

--- resources/views/teams/add.blade.php
+++ resources/views/teams/add.blade.php
@@ -226,11 +226,21 @@
                                 @endforeach
                                 $listAdd = "";
 
-                                for ($i = 0; $i < $listEmployeeID.length; $i++) {                                    
+                                for ($i = 0; $i < $listEmployeeID.length; $i++) {
+                                    $classBtr = '';
+                                    if($listEmployeeRole[$i] == 'PO'){
+                                        $classBtr = 'label label-primary';
+                                    } else if($listEmployeeRole[$i] == 'Dev'){
+                                        $classBtr = 'label label-success';
+                                    } else if($listEmployeeRole[$i] == 'BA'){
+                                        $classBtr = 'label label-info';
+                                    } else if($listEmployeeRole[$i] == 'ScrumMaster'){
+                                        $classBtr = 'label label-warning';
+                                    }
                                     $listAdd += "<tr id=\"show_" + $listEmployeeID[$i] + "\">"+
                                             "<td>" + $listEmployeeID[$i]+"</td>"+
                                             "<td>"+$listEmployeeTeam[$i]+"</td>"+
-                                            "<td>"+$listEmployeeRole[$i]+"</td>"+
+                                            "<td><span class=\""+ $classBtr +"\">"+$listEmployeeRole[$i]+"</span></td>"+
                                             "<td>" + $listEmployeeName[$i]+ "</td>"+
                                             "<td><a class=\"btn-employee-remove\"  style=\"margin-left: 25px;\"><i class=\"fa fa-remove\"  onclick=\"removeEmployee(" + $listEmployeeID[$i]+")\"></i></td></tr>";                              
                                 }
