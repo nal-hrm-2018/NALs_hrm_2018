27c32592411ed328ed684164da5f30d062cfd90d
hoangbuingocquy <hoangbuingocquy@gmail.com>
2018-05-22 16:48:05 +0700

    [ADD] create Controller and edit web.php

---
+++ app/Http/Controllers/Project/ProjectController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ngoc Quy
+ * Date: 5/7/2018
+ * Time: 10:51 AM
+ */
+namespace App\Http\Controllers\Project;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeamViewController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id, Request $request)
+    {
+    }
+}
