e4b961f5959df1c2412ccb9562f887f370c107a8
lbcong <lbcong15@gmail.com>
2018-06-18 10:33:12 +0700

    init-db-absence

---
+++ app/Models/Absence.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $last_updated_at
+ * @property int $last_updated_by_employee
+ * @property string $created_at
+ * @property int $created_by_employee
+ * @property string $delete_flag
+ * @property Employee[] $employees
+ */
+class Absence extends Model
+{
+    /**
+     * @var array
+     */
+
+    public $table = 'absences';
+    protected $fillable = [
+        'employee_id',
+        'absence_type_id',
+        'absence_status_id',
+        'from_date',
+        'to_date',
+        'reason',
+        'description',
+        'is_deny',
+        'is_late',
+        'is_summary',
+        'last_updated_at', 'last_updated_by_employee', 'created_at', 'created_by_employee', 'delete_flag'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function employee()
+    {
+        return $this->belongsTo('App\Models\Employee','employee_id');
+    }
+}
