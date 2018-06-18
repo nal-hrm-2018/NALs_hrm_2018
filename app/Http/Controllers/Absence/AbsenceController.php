72186aee0152420713aff0950fb844640c7b5c22
hoangbuingocquy <hoangbuingocquy@gmail.com>
2018-06-18 14:24:57 +0700

    [add] create new structure absence

--- app/Http/Controllers/Absence/AbsenceController.php
+++ app/Http/Controllers/Absence/AbsenceController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Absence;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class AbsenceController extends Controller
 {
+    public function __construct(){
 
+    }
+    public function index(Request $request){
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request){
+
+    }
+    public function show($id, Request $request){
+
+    }
+    public function edit($id){
+
+    }
+    public function update(Request $request, $id){
+
+    }
+    public function destroy($id, Request $request){
+
+    }
 }
---
+++ resources/views/absences/poteam.blade.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ngoc Quy
+ * Date: 4/16/2018
+ * Time: 11:00 AM
+ */ ?>
+@extends('admin.template')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Employee List
+                <small>Nal solution</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="{{asset('/dashboard')}}"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li><a href="{{asset('/employee')}}"> Employee</a></li>
+                <li><a href="#">List</a></li>
+            </ol>
+        </section>
+
+    </div>
+@endsection
