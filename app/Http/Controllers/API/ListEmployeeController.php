dcc93240bac3b2f0285d77b6155856e675fcc57d
DungLe1603 <dungle160396@gmail.com>
2018-07-30 15:26:42 +0700

    add file ProfileController

--- app/Http/Controllers/API/ListEmployeeController.php
+++ app/Http/Controllers/API/ListEmployeeController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\User;
+use App\employees;
+use App\Models\Employee;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Role;
+use JWTAuth;
+use JWTAuthException;
+use Hash;
 
 class ListEmployeeController extends BaseAPIController
 {
@@ -13,9 +20,47 @@ class ListEmployeeController extends BaseAPIController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = JWTAuth::toUser($request->token);
+        if($user->hasPermission("view_list_employee")) {
+            $employees = Employee::all();
+            foreach ($employees as $employees){
+                $role_id = $employees->role_id;
+                $role_name = Role::where('id', $role_id)->value('name');
+                $gender_id = $employees->role_id;
+                if ($gender_id == 1) {
+                    $gender_name = 'female';
+                }elseif ($gender_id == 2) {
+                    $gender_name = 'male';
+                }else{
+                    $gender_name = 'N/A';
+                }
+               $data[] = [
+                    'id' => $employees->id,
+                    'email'=>$employees->email,
+                    'name' => $employees->name,
+                    'birthday' => $employees->birthday,
+                    'gender' => [
+                        'gender_id' => $gender_id,
+                        'gender_name' => $gender_name
+                    ],
+                    'mobile' => $employees->mobile,
+                    'address' => $employees->address,
+                    'marital_status'=> $employees->marital_status,
+                    'startwork_date' => $employees->startwork_date,
+                    'endwork_date' => $employees->endwork_date,
+                    'role' => [
+                        'role_id' => $role_id,
+                        'role_name' => $role_name
+                    ],
+                ];
+            }
+            return $this->sendSuccess($data, 'list employees');
+        }        
+        else{
+            return $this->sendError(410, 'denied');
+        }
     }
 
     /**
@@ -38,9 +83,7 @@ class ListEmployeeController extends BaseAPIController
 
     public function show(Request $request)
     {
-        $user = JWTAuth::toUser($request->token);
-//        return response()->json(['result' => $user]);
-        return $this->sendSuccess($user);
+      
     }
 
     /**
@@ -63,6 +106,5 @@ class ListEmployeeController extends BaseAPIController
      */
     public function destroy($id)
     {
-        //
     }
 }
