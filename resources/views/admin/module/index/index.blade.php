5e0018bc6daa8498b0c3445259bc0040d45aecca
Hai Minh Ha <haminhhai95@gmail.com>
2018-08-21 13:52:39 +0700

    .

--- resources/views/admin/module/index/index.blade.php
+++ resources/views/admin/module/index/index.blade.php
@@ -2,49 +2,6 @@
 @section('content')
     <div class="content">
         <h6 class="title-content">Employees Information</h6>
-        <div class="box box-info">
-            <div class="box-header with-border">
-                <h3 class="box-title">Project</h3>
-
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
-                </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="table-responsive">
-                    <table class="table no-margin">
-                        <thead>
-                        <tr>
-                            <th>PROJECT ID</th>
-                            <th>NAME</th>
-                            <th>ROLE</th>
-                            <th>START DATE</th>
-                            <th>END DATE</th>
-                            <th>STATUS</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                            <td>Call of Duty IV</td>
-                            <td><span class="label label-success">PO</span></td>
-                            <td>2018-02-04</td>
-                            <td>2018-06-04</td>
-                            <td><span class="label label-success">in-progress</span></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.table-responsive -->
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer clearfix">
-                <a href="javascript:void(0)" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
-            </div>
-            <!-- /.box-footer -->
-        </div>
         <div class="container-donut-chart">
             <div class="child">
                     <span class="font-size-20">
