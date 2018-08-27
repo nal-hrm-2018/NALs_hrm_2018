5577b8c8e930aea08b30f77dddd71c7cf15a86f1
Trịnh Hùng Anh <anhthqb97@gmail.com>
2018-08-27 14:51:14 +0700

    update_lang_pdev

--- resources/views/admin/module/index/index.blade.php
+++ resources/views/admin/module/index/index.blade.php
@@ -677,7 +677,7 @@
                     <section>
                         <div class="box box-info">
                             <div class="box-header with-border">
-                                <h3 class="box-title">Project Developer</h3>
+                                <h3 class="box-title">{{trans('dashboard.project_dev')}}</h3>
 
                                 <div class="box-tools pull-right">
                                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
