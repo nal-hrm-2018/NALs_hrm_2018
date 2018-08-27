0fa10bd0974a9214431fe264d48fbd155a029efe
DungLe1603 <dungle160396@gmail.com>
2018-08-27 13:35:13 +0700

    [ADD]  header_chart change header new empployee chart

--- resources/views/admin/module/index/index.blade.php
+++ resources/views/admin/module/index/index.blade.php
@@ -279,7 +279,7 @@
                         <div class="col-md-4">
                             <div class="box box-danger height-400">
                                 <div class="box-header with-border">
-                                    <h3 class="box-title">{{trans('employee.new')}}</h3>
+                                    <h3 class="box-title">{{trans('employee.new')}}{{trans('employee.this_month')}}</h3>
 
                                     <div class="box-tools pull-right">
                                         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
