cb851621ab6dadfc04a01d304e4f2a9d472c6664
Trịnh Hùng Anh <anhthqb97@gmail.com>
2018-09-04 15:56:57 +0700

    update_function_add_overtime

--- resources/views/overtime/add.blade.php
+++ resources/views/overtime/add.blade.php
@@ -16,38 +16,52 @@
     		<div class="box box-default">
     			<div class="box-body">
 	    			<div class="col-md-12" style="width: 100% ; margin-bottom: 2em"></div>
-	    			<form action="{{asset('ot')}}" method="post">
+	    			<form action="{{asset('ot')}}" method="post" onsubmit="return confirmTime();">
 	    				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 	    				<div class="row">
 	    					<div class="col-md-1"></div>
 	    					<div class="col-md-4">
 								<div class="form-group">
 								    <label for="">{{trans('overtime.project')}}<strong style="color: red">(*)</strong></label>
-								    <select class="form-control" id="project_id" name="project_id">
-								    	<option value="">---{{trans('employee.drop_box.placeholder-default')}}---</option>
-								    	@foreach($objProject as $typeProject)
-								    	<option value="{{$typeProject->project_id}}">{{ \App\Models\Project::where('id',$typeProject->project_id)->first()->name }}
-								    	</option>
-								    	@endforeach
-								    </select>
+									@if($objProject->count() > 0)
+									<select class="form-control" value="{{ old('project_id') }}" id="project_id" name="project_id">
+										<option value="">---{{trans('employee.drop_box.placeholder-default')}}---</option>
+										@foreach($objProject as $typeProject)
+											<?php
+												$selected = "";
+												if($typeProject->project_id == old('project_id')){
+												    $selected = "selected";
+												}
+											?>
+											<option value="{{$typeProject->project_id}}" <?php echo $selected; ?>>{{ \App\Models\Project::where('id',$typeProject->project_id)->first()->name }}
+											</option>
+										@endforeach
+									</select>
+									@else
+									<select class="form-control" id="project_id" name="project_id" disabled>
+										<option value="">---{{trans('employee.drop_box.placeholder-default')}}---</option>
+										@foreach($objProject as $typeProject)
+											<option value="{{$typeProject->project_id}}">{{ \App\Models\Project::where('id',$typeProject->project_id)->first()->name }}
+											</option>
+										@endforeach
+									</select>
+									@endif
+
 									<label id="lb_error_project_id" style="color: red; ">{{$errors->first('project_id')}}</label>
 								</div>
 								<label for="">{{trans('overtime.date')}}<strong style="color: red">(*)</strong></label>
-								<div class="form-group input-group">
-									<div class="input-group-addon">
-                                            <i class="fa fa-calendar"></i>
-                                        </div>
-									<input type="date" class="form-control" id="date" name="date">
+								<div class="form-group">
+									<input type="date" class="form-control" id="date" name="date" value="{{ old('date') }}">
 									<label id="lb_error_date" style="color: red; ">{{$errors->first('date')}}</label>
 								</div>
 								<div class="form-group">
 									<label for="example3">{{trans('overtime.start_time')}}<strong style="color: red">(*)</strong></label>
-									<input type="text" class="form-control" id="start_time" name="start_time">
+									<input type="text" class="form-control" id="start_time" name="start_time" value="{{ old('start_time') }}">
 									<label id="lb_error_start_time" style="color: red; ">{{$errors->first('start_time')}}</label>
 								</div>
 								<div class="form-group">
 									<label for="example4">{{trans('overtime.end_time')}}<strong style="color: red">(*)</strong></label>
-									<input type="text" class="form-control" id="end_time" name="end_time">
+									<input type="text" class="form-control" id="end_time" name="end_time" value="{{ old('end_time') }}">
 									<label id="lb_error_end_time" style="color: red; ">{{$errors->first('end_time')}}</label>
 								</div>
 	    					</div>
@@ -55,22 +69,30 @@
 	    					<div class="col-md-4">
 	    						<div class="form-group">
 	    							<label for="">{{trans('overtime.total_time')}}<strong style="color: red">(*)</strong></label>
-	    							<input type="text" class="form-control" id="total_time" name="total_time">
+	    							<input type="number" class="form-control" id="total_time" name="total_time" value="{{ old('total_time') }}">
 									<label id="lb_error_total_time" style="color: red; ">{{$errors->first('total_time')}}</label>
 	    						</div>
-	    						<div class="form-group">
-	    							<label for="">{{trans('overtime.overtime_type_id')}}<strong style="color: red">(*)</strong></label>
-	    							<select class="form-control" id="overtime_type_id" name="overtime_type_id">
-								    	<option value="">---{{trans('employee.drop_box.placeholder-default')}}---</option>
-								    	@foreach($objOvertimeType as $typeTime)
-								    	<option value="{{$typeTime->id}}">{{$typeTime->name}}</option>
-								    	@endforeach
-								    </select>
-									<label id="lb_error_overtime_type_id" style="color: red; ">{{$errors->first('overtime_type_id')}}</label>
-	    						</div>
+								{{--@php--}}
+	    						{{--<div class="form-group">--}}
+	    							{{--<label for="">{{trans('overtime.overtime_type_id')}}<strong style="color: red">(*)</strong></label>--}}
+	    							{{--<select class="form-control" id="overtime_type_id" name="overtime_type_id" value="{{ old('overtime_type_id') }}">--}}
+								    	{{--<option value="">---{{trans('employee.drop_box.placeholder-default')}}---</option>--}}
+								    	{{--@foreach($objOvertimeType as $typeTime)--}}
+										{{--<?php--}}
+											{{--$selected = "";--}}
+                                            {{--if($typeTime->id == old('overtime_type_id')){--}}
+                                                {{--$selected = "selected";--}}
+                                            {{--}--}}
+										{{--?>--}}
+								    	{{--<option value="{{$typeTime->id}}" <?php echo $selected; ?>>{{$typeTime->name}}</option>--}}
+								    	{{--@endforeach--}}
+								    {{--</select>--}}
+									{{--<label id="lb_error_overtime_type_id" style="color: red; ">{{$errors->first('overtime_type_id')}}</label>--}}
+	    						{{--</div>--}}
+	    						{{--@endphp--}}
 	    						<div class="form-group">
 	    							<label for="">{{trans('overtime.reason')}}<strong style="color: red">(*)</strong></label>
-	    							<textarea class="form-control" id="" rows="6" style="line-height: 1.36;" name="reason"></textarea>
+	    							<textarea class="form-control" id="" rows="11" style="line-height: 1.36;" name="reason">{{ old('reason') }}</textarea>
 									<label id="lb_error_reason" style="color: red; ">{{$errors->first('reason')}}</label>
 	    						</div>
 	    					</div>
@@ -79,13 +101,13 @@
                             <div class="col-md-6" style="display: inline; ">
                                 <div style="float: right;">
                                     <button id="btn_reset_form_overtime" type="reset" class="btn btn-default"><span
-                                        class="fa fa-refresh"></span>&nbsp;RESET
+                                        class="fa fa-refresh"></span>&nbsp;{{ trans('common.button.reset')}}
                                     </button>
                                 </div>
                             </div>
                             <div class="col-md-1">
                                 <div>
-                                    <button type="submit" class="btn btn-info">ADD</button>
+                                    <button type="submit" class="btn btn-info">{{ trans('common.button.add')}}</button>
                                 </div>
                             </div>
                         </div>
@@ -94,6 +116,20 @@
     		</div>
     	</section>
     </div>
+	<script>
+		function confirmTime(){
+            var start_time = document.getElementById("start_time");
+            var end_time = document.getElementById("end_time");
+            var check = true;
+            if (start_time->format('H:i') > "09:00" && end_time->format('H:i') < "22:00"){
+				if (start_time->format('H:i') < end_time->format('H:i') < "22:00") {
+				    check = true;
+				}else{
+
+				}
+			}
+		}
+	</script>
     <script type="text/javascript">
     	var timepicker = new TimePicker(['start_time','end_time'], {
 		  	lang: 'en',
