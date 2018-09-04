75699ca8976d5ed29908e98c91081ec5b6d8103a
Trịnh Hùng Anh <anhthqb97@gmail.com>
2018-09-04 10:49:32 +0700

    add_function_create_overtime

---
+++ resources/lang/en/overtime.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dung Le
+ * Date: 8/30/2018
+ * Time: 8:55 AM
+ */
+return[
+    'notification' =>'notification',
+    'delete'=> 'delete',
+    'edit'=> 'Edit',
+    'add'=> 'Add',
+    'new'=> 'New Notifications',
+    'title' => 'Title',
+    'content' => 'Content',
+    'notification_id' => 'Notification Type',
+    'msg_add'=>[
+        'success'=>'Notification is successfully created !!!',
+        'fail'=>'Notification failed created!!!',
+        'fail_than_ten' =>'Notification failed created. You need to delete a new notification before adding it '
+    ],
+    'msg_edit'=>[
+        'success'=>'Notification successfully edit!!!',
+        'fail'=>'Notification failed edit!!!',
+    ]
+];
---
+++ resources/lang/vn/overtime.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dung Le
+ * Date: 8/30/2018
+ * Time: 8:55 AM
+ */
+return[
+    'project' =>'Project',
+    'date'=> 'Date',
+    'start_time'=> 'From time',
+    'end_time'=> 'To time',
+    'total_time'=> 'Number time',
+    'reason' => 'Title',
+    'overtime_type_id' => 'Date type',
+    'notification_id' => 'Notification Type',
+    'msg_add'=>[
+        'success'=>'Overtime is successfully created !!!',
+        'fail'=>'Overtime failed created!!!',
+    ],
+];
