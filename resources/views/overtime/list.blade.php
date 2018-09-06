3c33fad07a4127f6447d89bf8386002d456a3db9
congkhoa137 <38073571+congkhoa137@users.noreply.github.com>
2018-09-06 16:41:50 +0700

    Update list.blade.php

--- resources/views/overtime/list.blade.php
+++ resources/views/overtime/list.blade.php
@@ -10,14 +10,11 @@
         .width-90 {
             width: 90px;
         }
-        .form-inline {
-            display: inline;
-        }
     </style>
 
  <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <section class="content-header">
+	<div class="content-wrapper">
+		<section class="content-header">
             <h1>
                 My overtime
                 <small>NAL Solutions</small>
@@ -143,7 +140,7 @@
                                         <th>Date type</th>
                                         <th>Status</th>
                                         <th>Accept time</th>
-                                        <th>Actions</th>
+                                        <th colspan="2">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -183,20 +180,23 @@
                                             <td></td>
                                         @endif
                                         @if ($val->correct_total_time)
-                                            <td><span class="label label-success">{{$val->correct_total_time}} hours</span></td>
+                                            <td><span class="label label-primary">{{$val->correct_total_time}} hours</span></td>
                                         @else
                                             <td>-</td>
                                         @endif
                                         @if ($val->Status->name == 'Not yet')
                                         <td>
                                             <a class="btn btn-default" href="ot/{{$val->id}}/edit"><em class="fa fa-pencil"></em></a>
-                                             {{ Form::open(array('url' => ['/ot', $val["id"]], 'method' => 'delete', ['class' => 'form-inline'])) }}
+                                        </td>
+                                        <td>
+                                             {{ Form::open(array('url' => ['/ot', $val["id"]], 'method' => 'delete')) }}
                                                 <button type="submit" onclick="return confirm_delete();" class="btn btn-danger">
                                                     <em class="fa fa-trash"></em>
                                                 </button>
                                             {{ Form::close() }}
                                         </td>
                                         @else
+                                            <td>-</td>
                                             <td>-</td>
                                         @endif
                                     </tr>
@@ -206,7 +206,7 @@
                                         <td rowspan="3">Total</td>
                                         <td><span class="label" style="background: #9072ff;">Normal day</span></td>
                                         @if ($time['normal'])
-                                            <td><span class="label label-success">{{$time['normal']}} hours</span></td>
+                                            <td><span class="label label-primary">{{$time['normal']}} hours</span></td>
                                         @else
                                             <td>-</td>
                                         @endif
@@ -215,7 +215,7 @@
                                     <tr>
                                         <td><span class="label" style="background: #643aff;">Day off</span></td>
                                         @if ($time['weekend'])
-                                            <td><span class="label label-success">{{$time['weekend']}} hours</span></td>
+                                            <td><span class="label label-primary">{{$time['weekend']}} hours</span></td>
                                         @else
                                             <td>-</td>
                                         @endif
@@ -223,7 +223,7 @@
                                     <tr>
                                         <td><span class="label" style="background: #3600ff;">Holiday</span></td>
                                         @if ($time['holiday'])
-                                            <td><span class="label label-success">{{$time['holiday']}} hours</span></td>
+                                            <td><span class="label label-primary">{{$time['holiday']}} hours</span></td>
                                         @else
                                             <td>-</td>
                                         @endif
@@ -235,7 +235,7 @@
                 </div>
             </div>
         </section>
-    </div>
+	</div>
     <script>
         function confirm_delete(){
             return confirm(message_confirm('{{trans('common.action.remove')}}','form',''));
