9af1f6322823cd64b07fc8c39ae1199bdf0aab80
congkhuong <congkhuong1308@gmail.com>
2018-09-10 11:07:51 +0700

    aaa

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -9,6 +9,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" type="text/css" href="{!! asset('admin/templates/css/contain/common-dashboard.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('admin/templates/css/bower_components/font-awesome/css/font-awesome.min.css') !!}">
 
     <!-- Google Font -->
     {{--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">--}}
