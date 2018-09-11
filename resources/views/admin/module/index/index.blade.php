f0b9952932573452d1e1f4b07701a2fb8ab4e44c
congkhuong <congkhuong1308@gmail.com>
2018-09-11 09:13:40 +0700

    a

--- resources/views/admin/module/index/index.blade.php
+++ resources/views/admin/module/index/index.blade.php
@@ -370,7 +370,6 @@
                             <div class="box box-danger height-400">
                                 <div class="box-header with-border">
                                     <h3 class="box-title">{{trans('employee.new')}}{{trans('employee.this_month')}}</h3>
-
                                     <div class="box-tools pull-right">
                                         <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                         </button> -->
