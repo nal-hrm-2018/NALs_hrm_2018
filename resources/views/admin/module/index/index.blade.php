7d6414d4c74bfbbaa58433ec0ccdff5d09357108
congkhuong <congkhuong1308@gmail.com>
2018-09-11 15:34:23 +0700

    hide box-tools

--- resources/views/admin/module/index/index.blade.php
+++ resources/views/admin/module/index/index.blade.php
@@ -337,6 +337,7 @@
                                 </div>
                                 <!-- /.box-body -->
                             </div>
+                            
                         </div>
                         <div class="col-md-4">
                             <div class="box box-danger height-400">
@@ -598,7 +599,7 @@
                                 <h3 class="box-title">{{trans('dashboard.project')}}</h3>
 
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i> -->
                                     </button>
                                 </div>
                             </div>
@@ -657,8 +658,8 @@
                                 <h3 class="box-title">{{trans('dashboard.project_dev')}}</h3>
 
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                                    </button>
+                                    <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    </button> -->
                                 </div>
                             </div>
                             <!-- /.box-header -->
@@ -769,8 +770,8 @@
                                 <h3 class="box-title">{{trans('dashboard.project_dev')}}</h3>
 
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                                    </button>
+                                    <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    </button> -->
                                 </div>
                             </div>
                             <div class="box-body">
