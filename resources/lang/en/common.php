bab7616ffac3578a741e1fa241af236a4eafc3c0
DungLe1603 <dungle160396@gmail.com>
2018-09-16 22:52:44 +0700

    [modify]

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -98,7 +98,7 @@ return [
         'po_project'=>'PO Project',
         'confirmation'=>'Confirmation',
         'statistic'=>'Statistic',
-        'edit_overtime'=>'Edit overtime form',
+        'edit_overtime'=>'Edit overtime',
     ],
     'action'=>[
         'view'=>'View',
