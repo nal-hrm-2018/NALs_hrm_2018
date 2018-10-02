3959522441773e8b22422191365b1561e5953132
Trịnh Hùng Anh <anhthqb97@gmail.com>
2018-10-02 08:49:18 +0700

    clean_code

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -43,7 +43,8 @@ class DashboardController extends Controller
         // $this->objmNotification->deleteNotificationExpired();
         $id_emp = Auth::user()->id;
         $notifications = Notifications::where('end_date','>=',date('Y-m-d').' 00:00:00')
-        ->Where('delete_flag','0')->orderBy('id','desc')->get();
+                                        ->Where('delete_flag','0')
+                                        ->orderBy('id','desc')->get();
         $absences = Employee::emp_absence($id_emp);
         $notification_type = NotificationType::Where('delete_flag','0')->get();
         $overtime = Overtime::where('employee_id',$id_emp)
